ca1f0c381174116cae9a01cb4b7347d9715fadca
Giorgio Zoppi <sba23101@student.cct.ie>
2025-05-04 22:43:28 +0200

    new file:   extend_list.py

---
+++ pythonindepth/extend_list.py
@@ -0,0 +1,10 @@
+from collections import UserList
+
+class GoodList(UserList):
+    def __getitem__(self, index):
+        value = super().__getitem__(index)
+        if index % 2 == 0:
+            prefix = "even"
+        else:
+            prefix = "odd"
+        return f"[{prefix}] {value}"
